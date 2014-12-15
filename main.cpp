058d6b785bde0acbd43b3d35567a1ab34804d9ae
Ayushi <asinha8@jhu.edu>
2014-12-15 13:53:39 -0500

    Update: Fix Seg Fault

--- main.cpp
+++ main.cpp
@@ -17,41 +17,42 @@ void mexFunction( int nlhs , mxArray *plhs[] , int argc , const mxArray *prhs[] 
 		return ;
 	}
 	
-	char** argv = new char*[argc] ;
-	for ( int i = 0 ; i < argc ; i++ )
-	{
-		char* arg = (char *)mxArrayToString(prhs[i]) ;
-		argv[i] = strdup(arg) ;
-		mxFree(arg);
-	}
-	
-	cmdLineString In , Out ;
+	//char** argv = new char*[argc] ;
+	//for ( int i = 0 ; i < argc ; i++ )
+	//{
+	//	char* arg = (char *)mxArrayToString(prhs[i]) ;
+	//	argv[i] = strdup(arg) ;
+	//	printf("%s\n", argv[i]);
+	//	mxFree(arg);
+	//}
+	//
+	//cmdLineString In , Out ;
 
-	cmdLineReadable  APR  ;
-	
-	char* paramNames[]={
-		"in","out",
-		"axoplasmicreticula"
-	};
+	//cmdLineReadable  APR  ;
+	//
+	//char* paramNames[]={
+	//	"in","out",
+	//	"axoplasmicreticula"
+	//};
 
-	cmdLineReadable* params[]=
-	{
-		&In,&Out,
-		&APR
-	};
+	//cmdLineReadable* params[]=
+	//{
+	//	&In,&Out,
+	//	&APR
+	//};
 
-	//cmdLineParse(argc-1,&argv[1],paramNames,sizeof(paramNames)/sizeof(char*),params); // <-- CPP
-	cmdLineParse(argc,&argv[0],paramNames,sizeof(paramNames)/sizeof(char*),params); // <-- MATLAB
+	////cmdLineParse(argc-1,&argv[1],paramNames,sizeof(paramNames)/sizeof(char*),params); // <-- CPP
+	//cmdLineParse(argc,&argv[0],paramNames,sizeof(paramNames)/sizeof(char*),params); // <-- MATLAB
 
-	// Check that the input and output files have been set
-	if(!In.set || !Out.set){
-		if(!In.set)	{printf("Input image was not set\n");}
-		else		{printf("Output image was not set\n");}
-		ShowUsage("FindReticula") ;
-		return ; //EXIT_FAILURE;
-	}
+	//// Check that the input and output files have been set
+	//if(!In.set || !Out.set){
+	//	if(!In.set)	{printf("Input image was not set\n");}
+	//	else		{printf("Output image was not set\n");}
+	//	ShowUsage("FindReticula") ;
+	//	return ; //EXIT_FAILURE;
+	//}
 
-	if(APR.set){
+	//if(APR.set){
 
 		MatlabImage image(prhs[1]) ;
 		MatlabImage out(prhs[3]) ;
@@ -59,8 +60,8 @@ void mexFunction( int nlhs , mxArray *plhs[] , int argc , const mxArray *prhs[] 
 		if (!image.AxoplasmicReticula(image , out)) {
 			printf("Could not compute axoplasmic reticula in the image\n");
 		}
-		return ;
-	}
+		/*return ;
+	}*/
 
 	return ; //EXIT_SUCCESS;
 }
